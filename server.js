01bc2c4e0917f6018dfdc3ae83e74d43bec94be4
Kafaween <abod.kafaween@gmail.com>
2021-08-10 02:45:37 +0300

    done

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const jwksClient = require('jwks-rsa');
 const PORT = process.env.PORT;
 const JWKSURI = process.env.JWKSURI;
 app.use(cors());
+const mongoose = require('mongoose');
+const axios = require('axios');
 
 const client = jwksClient({
   jwksUri: JWKSURI,
@@ -36,5 +38,65 @@ app.get('/verify-token', (request, response) => {
     response.json(user);
   });
 });
+
+mongoose.connect('mongodb://localhost:27017/book_fav', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const BooksSchema = new mongoose.Schema({
+  title: String,
+  status: String,
+  description: String,
+  email: { type: String, unique: true },
+});
+const Book = mongoose.model('Book', BooksSchema);
+function SeedBooksCollection() {
+  const gameofthrones = new Book({
+    title: 'Game of thrones',
+    status: 'good',
+    description: 'a bunch of kings fighting on a chair',
+    email: 'm7moud.abubaker.92@gmail.com',
+  });
+  const harrypoter = new Book({
+    title: 'harry poter',
+    status: 'perfect',
+    description: 'some wizard stuffs',
+    email: 'deyaa.pozan@gmail.com',
+  });
+  const fightclub = new Book({
+    title: 'fightclub',
+    status: 'the best',
+    description: "don't talk about fight club (first rule)",
+    email: 'abod.kafaween@gmail.com',
+  });
+
+  gameofthrones.save();
+  harrypoter.save();
+  fightclub.save();
+}
+
+SeedBooksCollection();
+function handlebooks(req, res) {
+  console.log('dayaaaaa');
+  axios
+    .get(`http://localhost:8080/books?email=${req.query.email}`)
+    .then((resultemail) => {
+      console.log(req.query.email);
+      Book.findOne({ email: req.query.email }, function (error, result) {
+        if (error) {
+          res.send(error.message);
+        } else if (result === undefined) {
+          res.send('data is not found');
+        } else {
+          res.send(result);
+        }
+      });
+    })
+    .catch((error) => {
+      res.send(error.message);
+    });
+}
+
+app.get('/books', handlebooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
